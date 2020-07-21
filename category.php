df3cbfdf5da208ae4da53a0878c0d897f38afc5e
sergey <zavulon31990@gmail.com>
2020-07-21 14:13:54 +0300

    lesson 5

---
+++ category.php
@@ -0,0 +1,110 @@
+<?php get_header() ?>
+
+   <!-- Page Title
+   ================================================== -->
+   <div id="page-title">
+
+      <div class="row">
+
+         <div class="ten columns centered text-center">
+            <h1><span>.</span></h1>
+
+            <p>Aenean condimentum, lacus sit amet luctus lobortis, dolores et quas molestias excepturi
+            enim tellus ultrices elit, amet consequat enim elit noneas sit amet luctu. </p>
+         </div>
+
+      </div>
+
+   </div> <!-- Page Title End-->
+
+   <!-- Content
+   ================================================== -->
+   <div class="content-outer">
+
+      <div id="page-content" class="row">
+
+         <div id="primary" class="eight columns">
+            <!-- вывод постов -->
+   <?php if (have_posts()) {while (have_posts()) {the_post(); ?>
+      <article class="post">
+               <div class="entry-header cf">
+                  <!-- заголовок поста -->
+                  <h1><a href="<?php the_permalink(); ?>" title=""><?php the_title(); ?></a></h1>
+                  <!-- вывод даты и категорий -->
+                  <p class="post-meta"> 
+                     <time class="date" datetime="2014-01-14T11:24"><?php the_time('F jS, Y') ?></time> 
+                     <span class="categories"><?php the_category( $separator = '/', '') ?></span>
+                  </p>
+               </div>
+
+               <div class="post-thumb">
+                  <a href="<?php the_permalink(); ?>" title=""><?php the_post_thumbnail('post_thumb')?></a>
+               </div>
+
+               <div class="post-content">
+                  <!-- вывод всего contenta -->
+                  <?php the_content(); ?>
+
+               </div>
+
+            </article> <!-- post end -->
+     
+   <?php } //конец while ?>
+   <?php the_posts_pagination(); ?>
+   <?php } // конец if ?>
+            
+
+   
+
+
+
+         </div> <!-- Primary End-->
+
+         <div id="secondary" class="four columns end">
+
+        <?php get_sidebar() ?>
+
+         </div> <!-- Secondary End-->
+
+      </div>
+
+   </div> <!-- Content End-->
+
+   <!-- Tweets Section
+   ================================================== -->
+   <section id="tweets">
+
+      <div class="row">
+
+         <div class="tweeter-icon align-center">
+            <i class="fa fa-twitter"></i>
+         </div>
+
+         <ul id="twitter" class="align-center">
+            <li>
+               <span>
+               This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+               Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
+               <a href="#">http://t.co/CGIrdxIlI3</a>
+               </span>
+               <b><a href="#">2 Days Ago</a></b>
+            </li>
+            <!--
+            <li>
+               <span>
+               This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+               Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
+               <a href="#">http://t.co/CGIrdxIlI3</a>
+               </span>
+               <b><a href="#">3 Days Ago</a></b>
+            </li>
+            -->
+         </ul>
+
+         <p class="align-center"><a href="#" class="button">Follow us</a></p>
+
+      </div>
+
+   </section> <!-- Tweets Section End-->
+
+<?php get_footer() ?>
--- functions.php
+++ functions.php
@@ -1,8 +1,13 @@
 <?php
-
+// подключение стилей
 add_action( 'wp_enqueue_scripts', 'style_theme' );
+// подключение скриптов
 add_action( 'wp_enqueue_scripts', 'my_scripts_method');
+// регистрация меню
 add_action( 'after_setup_theme', 'registerMenu');
+// регистрация сайдбара
+add_action( 'widgets_init', 'register_my_widgets' );
+
 
 
 
@@ -29,4 +34,45 @@ function my_scripts_method() {
 function registerMenu() {
     register_nav_menu( 'top', 'главное меню' );
     register_nav_menu( 'bottom', 'меню в подвале' );
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails', array( 'post' ) );          // вывод превью Только для post
+    add_theme_support( 'post-thumbnails', array( 'page' ) ); // вывод превью Только для page
+    add_image_size('post_thumb', 1300, 500, true); //размер превью
+    // удаляет H2 из шаблона пагинации
+add_filter('navigation_markup_template', 'my_navigation_template', 10, 2 );
+function my_navigation_template( $template, $class ){
+	/*
+	Вид базового шаблона:
+	<nav class="navigation %1$s" role="navigation">
+		<h2 class="screen-reader-text">%2$s</h2>
+		<div class="nav-links">%3$s</div>
+	</nav>
+	*/
+
+	return '
+	<nav class="navigation %1$s" role="navigation">
+		<div class="nav-links">%3$s</div>
+	</nav>    
+	';
+}
+
+// выводим пагинацию
+the_posts_pagination( array(
+	'end_size' => 2,
+) ); 
+}
+
+
+function register_my_widgets(){
+	register_sidebar( array(
+		'name'          => 'right sidebar',
+		'id'            => "right_sidebar",
+		'description'   => 'Правый сайдбар',
+		'class'         => '',
+		'before_widget' => '<div class="widget">',
+		'after_widget'  => "</div>\n",
+		'before_title'  => '<h5 class="widgettitle">',
+		'after_title'   => "</h5>\n",
+    ) );
+   
 }
